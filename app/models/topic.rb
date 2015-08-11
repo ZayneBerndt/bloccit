94eef3417e8c21b78bd1912647cc2724f5d216de
Zayne Berndt <berndt.zayne@gmail.com>
2015-08-11 22:50:48 +1200

    Interlude Assignment

--- app/models/topic.rb
+++ app/models/topic.rb
@@ -2,7 +2,5 @@ class Topic < ActiveRecord::Base
 has_many :posts
 
    validates :title, length: { minimum: 5 }, presence: true
-   validates :body, length: { minimum: 20 }, presence: true
    
-   validates :user, presence: true
 end
