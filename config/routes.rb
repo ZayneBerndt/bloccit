e3958953399ddae8be4e4260556f74d15519f0be
Zayne Berndt <berndt.zayne@gmail.com>
2015-08-24 16:15:16 +1200

    AS - Destroy

--- config/routes.rb
+++ config/routes.rb
@@ -8,15 +8,18 @@ Rails.application.routes.draw do
 
   get 'comments/edit'
 
- devise_for :users
- resources :users, only: [:update]
- resources :questions
- resources :advertisements
+    devise_for :users
+     resources :users, only: [:update]
+     resources :questions
+     resources :advertisements
  
- resources :topics do
-     resources :posts, except: [:index] do
-     resources :comments, only: [:create, :destroy]
-
+     resources :topics do
+     resources :posts, except: [:index]
+   end
+   resources :posts, only: [] do
+   resources :comments, only: [:create, :destroy]
+   end
+     
      post '/up-vote' => 'votes#up_vote', as: :up_vote
      post '/down-vote' => 'votes#down_vote', as: :down_vote
     end
