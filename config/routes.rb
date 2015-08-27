a1ecc2ed6ee2c2270772cc88ebb52462ddfed4ef
Zayne Berndt <berndt.zayne@gmail.com>
2015-08-28 06:27:48 +1200

    Debug

--- config/routes.rb
+++ config/routes.rb
@@ -1,40 +1,34 @@
 Rails.application.routes.draw do
-  
-  get 'comments/create'
 
-  get 'comments/show'
-
-  get 'comments/destroy'
-
-  get 'comments/edit'
-
-    devise_for :users
-     resources :users, only: [:update]
-     resources :questions
-     resources :advertisements
+  devise_for :users
+  resources :users, only: [:update]
+  resources :questions
+  resources :advertisements
  
 
- resources :topics do
-     resources :posts, except: [:index] do
-     resources :comments, only: [:create, :destroy]
+  resources :topics do
+    resources :posts, except: [:index] #do
+    #   resources :comments, only: [:create, :destroy]
+    # end
+  end
 
 
-     resources :topics do
-     resources :posts, except: [:index]
-   
-   resources :posts, only: [] do
-   resources :comments, only: [:create, :destroy]
-     post '/up-vote' => 'votes#up_vote', as: :up_vote
-     post '/down-vote' => 'votes#down_vote', as: :down_vote
-    end
+  # resources :topics do
+  #   resources :posts, except: [:index]
+
+  resources :posts, only: [] do
+    resources :comments, only: [:create, :destroy]
+    post '/up-vote' => 'votes#up_vote', as: :up_vote
+    post '/down-vote' => 'votes#down_vote', as: :down_vote
+  end
 
   get 'welcome/contact'
 
- get 'about' => 'welcome#about'
+  get 'about' => 'welcome#about'
 
- get 'advertisement' =>'posts#advertisement'
+  get 'advertisement' =>'posts#advertisement'
 
- get 'question' => 'question#new'
+  get 'question' => 'question#new'
 
   root to: "welcome#index"
 
