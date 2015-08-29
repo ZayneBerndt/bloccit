1b4440637340dc8629e02e3d4e41897ceca1dc52
Zayne Berndt <berndt.zayne@gmail.com>
2015-08-29 16:07:43 +1200

    markdown bug fix

--- app/models/post.rb
+++ app/models/post.rb
@@ -55,18 +55,18 @@ def update_rank
   def save_with_initial_vote
   end
  
- def markdown_to_html(title)
+ def markdown_to_html
 
   render_as_markdown(self.title)
   end
   
-  def markdown_to_html(body)
+  def markdown_to_html
    render_as_markdown(self.body)
   end
 
 private 
 
-    def markdown_to_html(markdown)
+    def markdown_to_html
         renderer = Redcarpet::Render::HTML.new
         extensions = {fenced_code_blocks: true}
         redcarpet = Redcarpet::Markdown.new(renderer, extensions)
