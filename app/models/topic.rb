c3d457c7ad09c90f5cc95784b5a42bd022302fb4
Zayne Berndt <berndt.zayne@gmail.com>
2015-09-07 22:51:19 +1200

    AS - Private Topics

--- app/models/topic.rb
+++ app/models/topic.rb
@@ -1,10 +1,21 @@
 class Topic < ActiveRecord::Base
  has_many :posts, dependent: :destroy
 
- scope :visible_to, -> (user) { user ? all : where(public: true) }
+ scope :visible_to, -> (user) {user ? all : :publicly_viewable}
 
    validates :title, length: { minimum: 5 }, presence: true
 
   WillPaginate.per_page = 50
-   
+
+  def publicly_viewable(user)
+    if user
+      topic_collection.all
+    else
+      topic_collection.where(public: true)
+    end
+   end
+
+    def privately_viewable
+     topic_collection.where(public: false)
+    end
 end
