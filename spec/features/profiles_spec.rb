35f4d1ac1f77d427f0bf2e0a38b919eef98e99af
Zayne Berndt <berndt.zayne@gmail.com>
2015-09-14 00:06:28 +1200

    AS - Public-Profiles

--- spec/features/profiles_spec.rb
+++ spec/features/profiles_spec.rb
@@ -20,6 +20,25 @@ describle "Visiting profiles" do
         expect( page ).to have_content(@post.title)
         expect( page ).to have_content(@comment.body)
     end
+   end 
 
-    end 
+   include Warden::Test::Helpers
+
+   describle "Visiting profiles" do 
+
+    before do
+      login_as(@user, :scope => :user)
+    end
+    
+     describe " signed in" do
+       
+       it "shows profile" do
+       visit user_path(@user)
+       expect(current_path).to eq(user_path(@user))
+       expect( page ).to have_content(@user.name)
+       expect( page ).to have_content(@post.title)
+       expect( page ).to have_content(@comment.body)
+     end
+   end 
+
 end      
