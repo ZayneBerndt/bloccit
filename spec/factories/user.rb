6b294168ffb9104c878e7454414b712e21645fb1
Zayne <berndt.zayne@gmail.com>
2015-09-15 20:46:06 +1200

    CP - Testing Users

---
+++ spec/factories/user.rb
@@ -0,0 +1,23 @@
+FactoryGirl.define do
+   factory :user do
+     name "Douglas Adams"
+     sequence(:email, 100) { |n| "person#{n}@example.com" }
+     password "helloworld"
+     password_confirmation "helloworld"
+     confirmed_at Time.now
+   end
+   factory :user_with_post_and_comment do
+        transient do
+        posts_count 
+        comments_count 
+      
+
+      after(:build) do |user, evaluator|
+        create_list(:post, evaluator.posts_count, user: user)
+        create_list(:comment, evaluator.comments_count, user: user)
+        
+        end 
+      end
+   end
+
+
